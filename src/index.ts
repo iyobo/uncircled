11432cecf88b78f1cb1d5db906017c1b9867e27f
iyobo <phil.eki@gmail.com>
2019-09-11 06:45:36 -0500

    Fixed marshalling for empty target[key] when it is an object type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 function toJSON() {
     const obj = {};
     const keys = Object.keys(this);
@@ -15,31 +14,50 @@ function toJSON() {
     return obj;
 }
 
-
-function deserialize(rawObject: string | any) {
-
-    if(typeof rawObject=== 'string') {
-        rawObject = JSON.parse(rawObject);
-    }
+const fill= (target, rawObject: string | any)=> {
 
     const keys = Object.keys(rawObject);
+    // console.log('rawObject', rawObject);
 
     for (const key of keys) {
-        const dateFields = this['_$$uncircleDateFields'] || {};
+        const dateFields = target['_$$uncircleDateFields'] || {};
         const field = rawObject[key];
 
-        if (field ) {
-            if( dateFields[key]) {
-                this[key] = new Date(field);
-            }
-            else if( typeof field == 'object' && field.constructor && field.constructor.name !== 'Object') { // raw object, not a class instance
-                deserialize.bind(this[key])(field);
-            }
-            else if(typeof field !== 'function') {
-                this[key] = field;
+        if (field !== null && field !== undefined) {
+            // console.log(' key:', key,
+            //     'field:', field,
+            //     'self:', target[key],
+            //     'typeof self:', typeof target[key],
+            //     'typeof field:', typeof field);
+
+            if (dateFields[key]) {
+                // console.log('  date', field);
+                target[key] = new Date(field);
+                // } else if (typeof field == 'object' && field.constructor && field.constructor.name !== 'Object') { // raw object, not a class instance
+            } else if (typeof field == 'object' || !field.constructor ) { // raw object, not a class instance
+                if(Object.keys(field).length=== 0) {
+                    // this is empty. ignore
+                    // console.log('  empty field object. ignoring');
+                    continue;
+                }
+
+                // console.log('  nesting', key, field);
+                if(!target[key]) target[key] = {};
+                fill(target[key], field);
+            } else {
+                // console.log('  Nothing special. probably a primitive', field);
+                target[key] = field;
             }
         }
     }
+};
+
+function deserialize(rawObject: string | any) {
+    let raw = rawObject;
+    if (typeof rawObject === 'string') {
+        raw = JSON.parse(rawObject);
+    }
+    fill(this, raw);
 }
 
 
